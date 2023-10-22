9d8f0370be29ebe427595a8a628820652164c8a9
Sarthak Shrestha <sarthak3926@gmail.com>
2023-10-22 15:14:49 -0500

    more matrix manipulation

--- matrix.cpp
+++ matrix.cpp
@@ -5,28 +5,80 @@
 
 using namespace std;
 
-void genShowMatrix(){
+//
+void genShowMatrix(int matrix[5][5], int rows , int column){
+
+    // const int rows = 5;
+    // const int column = 5;
+    // int matrix[rows][column];
+
+    for (int i = 0; i < rows; i++){
+        for(int j = 0; j < column; j++){
+            matrix[i][j] = -1;
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+//
+void genHideMatrix(int hiddenMatrix[5][5],int lowerBound = 5, int upperBound =5){
+
+    
+    srand((time(0)));
+    const int rows = 5;
+    const int column = 5;
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < column; j++){
+            hiddenMatrix[i][j] = (rand()%(1 + upperBound - lowerBound)) + lowerBound;
+            cout << hiddenMatrix[i][j] << "\t";
+        }
+        cout << endl;
+        
+    }
+    
+
+}
+
+//
+void initialize(int hiddenMatrix[5][5],int displayedLowerBound = -1, int displayedUpperBound = -1){
+
+    //generating upper bound and lower bound for guess number generation range
+    int lowerBound = (rand()%(100) + 100);
+    int upperBound = (rand()%(100) + 200);
+
+    cout << displayedLowerBound << "\t\t" << displayedUpperBound << endl;
+    
+    cout << lowerBound << "\t\t" << upperBound << endl;
+
+    genHideMatrix(hiddenMatrix ,lowerBound, upperBound);
+
+
+}
+
+int main(){
+
+    srand(time(0));
+    int displayedLowerBound = -1;
+    int displayedUpperBound = -1;
 
     const int rows = 5;
     const int column = 5;
     int matrix[rows][column];
+    int hiddenMatrix[rows][column];
+    
+    initialize(hiddenMatrix);
 
+    cout << endl;
 
     for (int i = 0; i < rows; i++){
-
-        for(int j = 0; j < column; j++){
-            matrix[i][j] = 0;
-            cout << matrix[i][j] << "\t";
+        for (int j = 0; j < column; j++){
+            cout << hiddenMatrix[i][j] << "\t";
         }
-           cout << endl;
+        cout << endl;
+        
     }
-
-}
-
-
-int main(){
-
-    genShowMatrix();
 
     return 0;
 
