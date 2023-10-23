b33ac80242b4b92831f7342e64a78001782031c3
Sarthak Shrestha <sarthak3926@gmail.com>
2023-10-23 12:43:53 -0500

    minor editing and optimization

--- matrix.cpp
+++ matrix.cpp
@@ -28,7 +28,7 @@ void genHideMatrix(int hiddenMatrix[5][5],int lowerBound = 5, int upperBound =5)
     srand((time(0)));
     const int rows = 5;
     const int column = 5;
-
+/*
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < column; j++){
             hiddenMatrix[i][j] = (rand()%(1 + upperBound - lowerBound)) + lowerBound;
@@ -38,6 +38,7 @@ void genHideMatrix(int hiddenMatrix[5][5],int lowerBound = 5, int upperBound =5)
         
     }
     
+*/
 
 }
 
@@ -46,7 +47,9 @@ void initialize(int hiddenMatrix[5][5],int displayedLowerBound = -1, int display
 
     //generating upper bound and lower bound for guess number generation range
     int lowerBound = (rand()%(100) + 100);
+    //int lowerBound = (rand()%(100) + 100);
     int upperBound = (rand()%(100) + 200);
+    //int upperBound = (rand()%(100) + 200);
 
     cout << displayedLowerBound << "\t\t" << displayedUpperBound << endl;
     
@@ -61,7 +64,9 @@ int main(){
 
     srand(time(0));
     int displayedLowerBound = -1;
+    int lowerBound = 0;
     int displayedUpperBound = -1;
+    int upperBound = 0;
 
     const int rows = 5;
     const int column = 5;
@@ -70,6 +75,7 @@ int main(){
     
     initialize(hiddenMatrix);
 
+/*
     cout << endl;
 
     for (int i = 0; i < rows; i++){
@@ -79,6 +85,10 @@ int main(){
         cout << endl;
         
     }
+*/    
+
+    displayedLowerBound = lowerBound;
+    cout << displayedLowerBound << endl; 
 
     return 0;
 
